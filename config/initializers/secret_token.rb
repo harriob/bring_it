f47ff5872c6243f0f8370fc5cd694fd83934a1a3
Harriet Owers-Bradley <h.owersbradley@gmail.com>
2013-05-12 15:02:23 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BringIt::Application.config.secret_token = '18fa2cce5c210001da5e60f01d76ca40d4cbd92f8568834cbe4049aa43f37bc43dc1fa9797c8e03922067efe52ddc6392c58c20e13a9b6ba0cf8cd4c04b164bf'
